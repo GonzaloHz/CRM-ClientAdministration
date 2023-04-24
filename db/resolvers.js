5541a31feb3f46aa0f22eca3984e48b0d2f313f0
Gonzalo Hernandez <gonzalo.hernandez1@hotmail.com>
2023-04-23 22:33:40 -0300

    Create user models and add users to the db

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,6 +1,27 @@
+const User = require('../models/User')
+
 const resolvers = {
     Query: {
         getCourses: () => "hi"
+    },
+    Mutation: {
+        newUser: async (_, {input}, ctx) => {
+            const { email } = input;
+
+            //Check if the user was already register
+            const registeredUser = await User.findOne({email})
+            if(registeredUser) throw new Error('The user has already exists')
+
+            //Save it on db
+            try {
+                const newUser = new User(input);
+                newUser.save();
+                return newUser;
+            } catch (error) {
+                console.log(error);
+            }
+
+        }
     }
 }
 
