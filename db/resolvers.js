739b840560cd074b043d073c39b1b74200b1abbd
Gonzalo Hernandez <gonzalo.hernandez1@hotmail.com>
2023-04-23 22:47:27 -0300

    hash the password of the users

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const bcryptjs = require('bcryptjs')
 
 const resolvers = {
     Query: {
@@ -6,11 +7,15 @@ const resolvers = {
     },
     Mutation: {
         newUser: async (_, {input}, ctx) => {
-            const { email } = input;
+            const { email, password } = input;
 
             //Check if the user was already register
             const registeredUser = await User.findOne({email})
             if(registeredUser) throw new Error('The user has already exists')
+
+            //Hash the password
+            let salt = await bcryptjs.genSalt(10);
+            input.password = await bcryptjs.hash(password, salt)
 
             //Save it on db
             try {
